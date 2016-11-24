6f1e83988826da8e76284c1c8c09f7a59c1dce15
tahmidefaz <tahmidefaz@gmail.com>
2016-11-23 21:05:16 -0500

    added anyfib.py

---
+++ anyfib.py
@@ -0,0 +1,32 @@
+#############################################################################
+# Author: Tahmid Efaz
+# Date: 11/23/16
+# Purpose: Given two numbers of a sequence, it will compute the
+#          nth fib term of the sequence
+# Speciality: option to override python's built in maximum recursion depth
+#############################################################################
+
+import sys
+sys.setrecursionlimit(10000)        # override python's default recursion depth (Change this number as needed)
+
+
+def anyfib(n2,n1,term):
+    '''
+    Given two numbers (n1 and n2) it will compute the fib number
+    based on the term
+    '''
+
+    if term == 0:
+        return n2
+    else:
+        return anyfib(n2+n1,n2,term-1)
+
+
+def main():
+    n1 = abs(int(raw_input("Enter the first number in the series: ")))
+    n2 = abs(int(raw_input("Enter the second number of the series: ")))
+    num = abs(int(raw_input("Which fibonacci term you want to compute?(*Term starts from 0) : ")))
+    fib = anyfib(n1,n2, num)
+    print "Result from minifib is: " + str(fib)
+
+main()
